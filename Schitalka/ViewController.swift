225199ef27699fb519364b8a9d3bb47200fa4daf
Bakha <adamovbaha@gmail.com>
2023-08-29 17:20:11 +0300

    Не знаю правильно ли все сделал

--- Schitalka/ViewController.swift
+++ Schitalka/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var count = 0 {
+        didSet {
+            textLabel.text = "Значение счётчика:\(count)"
+        }
+    }
+    
+    @IBAction func titleButton(_ sender: UIButton) {
+        count += 1
+        textLabel.text = "Значение счётчика: \(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
